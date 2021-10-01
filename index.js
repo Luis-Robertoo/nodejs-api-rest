6b255733ade5f71f699988d45b2b80c29109bbfc
Luis <37544328+Luis-Robertoo@users.noreply.github.com>
2021-10-01 18:22:46 -0300

    teste com heroku

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 
 const app = express()
 
@@ -6,3 +6,14 @@ app.listen(3000, () => {console.log('Servidor rodando na porta 3000')})
 
 app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e esta realizando um GET'))
 
+*/
+
+const express = require('express')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+  
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+app.get('/', (req, res) => res.send('Você está na rota de atendimentos e esta realizando um GET'))
